e94c72cdcfd7dad1dc2a19b736d1e1e832579a16
Matheus Leichsnering <matheusleichs@hotmail.com>
2017-03-17 01:41:26 +0000

    inclusão da biblioteca locale.h para suporte do portugês para windons

--- CI/capitulo4-final/adivinhacao.c
+++ CI/capitulo4-final/adivinhacao.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
 
 int main() {
-
+    setlocale (LC_ALL, "portuguese");
+    
     // imprime cabecalho do nosso jogo
     printf("                                                                         \n");
     printf("                                 ,.=,,==. ,,_                            \n");
